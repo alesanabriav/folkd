6044ddace4355a80d3ee693c633e3f35db5d8ca1
Alejandro Sanabria <alesanabriav@gmail.com>
2017-08-30 18:30:31 -0500

    add validation

--- server.js
+++ server.js
@@ -47,8 +47,8 @@ app.prepare()
 
     if(params.type === 'team') {
       return findOrCreateTeam(body)
-      .then(user => res.status(201).json(user))
-      .catch(err => res.status(400).json(err));
+        .then(user => res.status(201).json(user))
+        .catch(err => res.status(400).json(err));
     }
 
     if(params.type === 'company') {
@@ -58,12 +58,30 @@ app.prepare()
     }
 
     if(params.type === 'user') {
-      return models.User.create(body)
+      return models.Company.findOne({ where: {id: body.company_id}, include: [{ model: models.User, fields: ['id'] }] })
+        .then((company) => {
+          let userData = body;
+
+          if(company.users.length === 0) {
+            userData = Object.assign(body, {role: 'owner'});
+            const domain = userData.email ? userData.email.split('@')[1] : '';
+            return company.update({ domain }).then(() => {
+              return userData
+            });
+          } else {
+            return new Promise((resolve, reject) => {
+              userData = Object.assign(body, {role: 'member'});
+              return resolve(userData);
+            });
+          }
+        })
+        .then(userData => models.User.create(userData))
         .then(user => {
           const token = getToken(user);
           sendMail(user);
           delete user.password;
           delete user.verify_token;
+
           res.status(201).json({token, user});
         })
         .catch(err => res.status(400).json(err));
